d5ed3e71a6276e9875bfc4c106d7a5513d05c6af
Dennis Durairaj <dennis.durairaj@gmail.com>
2019-06-30 09:43:19 +0200

    Add suffix reducer to reducer states

--- src/store.js
+++ src/store.js
@@ -2,13 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 
-import users from './state/users';
-import repos from './state/repos';
+import usersReducer from './state/users';
+import reposReducer from './state/repos';
 import fetching from './state/fetching';
 
 const rootReducer = combineReducers({
-  users,
-  repos,
+  usersReducer,
+  reposReducer,
   fetching
 });
 
