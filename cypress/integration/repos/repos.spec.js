61b4369270bb7cb3708788a12f6a4174799b8e8c
Dennis Durairaj <dennis.durairaj@pl.pwc.com>
2019-07-03 18:58:44 +0200

    Remove unwanted cypress example files

---
+++ cypress/integration/repos/repos.spec.js
@@ -0,0 +1,58 @@
+/// <reference types="cypress" />
+
+describe("Repos", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  context("When page opens", () => {
+    it("should focus on search input", () => {
+      cy.get('input[type="text"]').should("be.focused");
+    });
+  });
+
+  context("When there are no items", () => {
+    it("should not display list", () => {
+      cy.get("li").should("not.exist");
+    });
+  });
+
+  context("Fetch user repos successfully", () => {
+    beforeEach(() => {
+      cy.get('input[type="text"]')
+        .click()
+        .type("typicode");
+      cy.get('input[type="submit"]').click();
+    });
+    it("should display list of repos", () => {
+      cy.get("li").should("exist");
+    });
+    it("should not display error", () => {
+      cy.get(".error").should("not.exist");
+    })
+  });
+
+  context("Fetch user repos failure", () => {
+    const searchUser = "dennisdurairaj001";
+    beforeEach(() => {
+      cy.get('input[type="text"]')
+        .click()
+        .type(`${searchUser}`);
+      cy.get('input[type="submit"]').click();
+      cy.server();
+      cy.route({
+        url: `https://api.github.com/users/${searchUser}`,
+        method: "GET",
+        status: 404,
+        response: {}
+      });
+    });
+
+    it("should display error", () => {
+      cy.get(".error").should("exist");
+    });
+    it("should not display list", () => {
+      cy.get("li").should("not.exist");
+    });
+  });
+});
